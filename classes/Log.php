b29b8b6fe5ba005a79465972bd4322321067f9a5
mert <mert@mert.su>
2015-12-20 22:38:00 +0300

    favicon and small fixs

---
+++ classes/Log.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mert
+ * Date: 12/20/15
+ * Time: 7:19 AM
+ */
+
+class Log
+{
+    public static function write($text)
+    {
+        $text = rand(1000, 9999).' '.date('c') . "\t" . $text . PHP_EOL;
+
+        if(!file_exists(HIRVI_LOGS)) {
+            if(!mkdir(HIRVI_LOGS)){
+                die('cant create directory '.HIRVI_LOGS);
+            }
+        }
+        file_put_contents(HIRVI_LOGS . '/log.txt', $text, FILE_APPEND);
+    }
+}
